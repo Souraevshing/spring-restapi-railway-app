c178635e243c61948586a25420b48842d3089361
Sourav Kumar <isauravshing@gmail.com>
2023-09-23 21:48:50 +0530

    Add files via upload

---
+++ src/main/java/springrestapii/demo/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package springrestapii.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("springrestapii.demo"))
+                .paths(PathSelectors.any())
+                .build()
+                .protocols(new HashSet<>(Arrays.asList("https"))); // Specify HTTPS protocol
+    }
+
+}
