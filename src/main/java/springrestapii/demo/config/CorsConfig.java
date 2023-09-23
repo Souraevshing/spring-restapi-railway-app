cfed3f7515e6ad4599b18e572f309291be600558
Sourav Kumar <isauravshing@gmail.com>
2023-09-23 22:41:58 +0530

    Create CorsConfig.java

---
+++ src/main/java/springrestapii/demo/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package springrestapii.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Allow all origins and methods for Swagger UI
+        registry.addMapping("/swagger-ui.html")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true); // Allow credentials (e.g., cookies)
+    }
+
+}
